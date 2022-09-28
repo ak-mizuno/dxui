bc8f90e60f6ded17d97bc85fc9dcc3ebaef0952b
Akihiro MIZUNO <ak-mizuno@toyotasystems.com>
2022-09-28 16:13:11 +0900

    recover

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,5 @@ module.exports = {
     devServer: {
         port: 3000,
         disableHostCheck: true,
-        proxy: {
-            "/api/": {
-                target: "https://orange-desert-05ffc270f.2.azurestaticapps.net/api/",
-            }
-        }
     },
 };
